99616c299004caefec032827066e27afa960f552
al.tomilin <alesha.tomilin2014@gmail.com>
2023-12-15 17:18:25 +0300

    csrf

--- wigets/settings.py
+++ wigets/settings.py
@@ -140,4 +140,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = []
 if csrf_subdomain := os.getenv('CSRF_SUBDOMAIN'):
-    CSRF_TRUSTED_ORIGINS += [f'http://{csrf_subdomain}', f'https://{csrf_subdomain}']
+    CSRF_TRUSTED_ORIGINS += [f'http://{csrf_subdomain}', f'https://{csrf_subdomain}', 'https://widget.tomilin.pro']
