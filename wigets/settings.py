77f807d3291eb07af624aab27a7cdb6239c0c4c8
al.tomilin <alesha.tomilin2014@gmail.com>
2023-12-15 17:05:05 +0300

    deploy

--- wigets/settings.py
+++ wigets/settings.py
@@ -136,3 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = []
+if csrf_subdomain := os.getenv('CSRF_SUBDOMAIN'):
+    CSRF_TRUSTED_ORIGINS += [f'http://{csrf_subdomain}', f'https://{csrf_subdomain}']
